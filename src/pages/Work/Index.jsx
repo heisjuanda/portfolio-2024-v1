2151e03e1cc2b7ac85b186e6e0bd6014b034ffbb
Juan David Moreno Alfonso <juandavid@cressco.dev>
2024-01-02 12:29:58 -0500

    add new functions to work component and style

--- src/pages/Work/Index.jsx
+++ src/pages/Work/Index.jsx
@@ -26,6 +26,19 @@ const Work = () => {
       ease: "power2.inOut",
     });
 
+    gsap.to(
+      ".container--title",
+      {
+        duration: 1,
+        translateY: "100%",
+        ease: "power4.inOut",
+        stagger: {
+          amount: 0.1
+        }
+      },
+      "<"
+    );
+
     setTimeout(() => {
       history(`/work/${project.handle}`);
     }, 1001);
@@ -73,29 +86,64 @@ const Work = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useLayoutEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to(".container--title", {
+      duration: 1,
+      delay: 0.5,
+      translateY: 0,
+      ease: "power4.inOut",
+      stagger: {
+        amount: 0.1
+      }
+    });
+
+    tl.to(
+      "article",
+      {
+        duration: 1,
+        delay: 0.5,
+        opacity: 1,
+        ease: "power4.inOut",
+      },
+      "<"
+    );
+  }, []);
+
   return (
     <main className="work-section">
       <section className="work-section__container">
-        {WORK_PROJECTS.length > 0 &&
-          WORK_PROJECTS.map((project) => (
-            <div key={project.key} className="item">
-              <div className="item-text">
-                <h1>{project.name}</h1>
+        <header>
+          <h2>
+            <span className="container--title">Selected</span>
+          </h2>
+          <h2>
+            <span className="container--title">Projects</span>
+          </h2>
+        </header>
+        <article>
+          {WORK_PROJECTS.length > 0 &&
+            WORK_PROJECTS.map((project) => (
+              <div key={project.key} className="item">
+                <div className="item-text">
+                  <h1>{project.name}</h1>
+                </div>
+                <div
+                  className="item-img"
+                  onClick={() => {
+                    handleClickProject(project);
+                  }}
+                >
+                  <img
+                    src={project.mainPicutre}
+                    alt="project poster"
+                    loading="eager"
+                  />
+                </div>
               </div>
-              <div
-                className="item-img"
-                onClick={() => {
-                  handleClickProject(project);
-                }}
-              >
-                <img
-                  src={project.mainPicutre}
-                  alt="project poster"
-                  loading="eager"
-                />
-              </div>
-            </div>
-          ))}
+            ))}
+        </article>
       </section>
     </main>
   );
